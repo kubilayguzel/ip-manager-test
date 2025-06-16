acb9c3ec3ce1be0a164fbc4c5ab6836bf14cee2c
kubilayguzel <mkubilayguzel@gmail.com>
2025-06-16 23:43:48 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -970,4 +970,4 @@ export async function createDemoData() {
 
 
 // --- Exports ---
-export { auth, db, authService, personsService, ipRecordsService, accrualService };
+export { auth, db, authService, personsService, accrualService };
