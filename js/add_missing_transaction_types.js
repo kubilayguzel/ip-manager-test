4e83c7b765b06be5d30e2d02ac9e204a2a75931a
kubilayguzel <mkubilayguzel@gmail.com>
2025-06-26 12:45:47 +0300

    Update add_missing_transaction_types.js

--- js/add_missing_transaction_types.js
+++ js/add_missing_transaction_types.js
@@ -43,3 +43,6 @@ async function addMissingChildTransactionTypes() {
 
 // Fonksiyonu başlat
 addMissingChildTransactionTypes();
+
+
+        <script type="module" src="./js/add_missing_transaction_types.js"></script>
