b857eed9d398fb0af362f9facb16b76a82f9bc8e
kubilayguzel <mkubilayguzel@gmail.com>
2025-07-22 22:06:13 +0300

    Create run-search.js

---
+++ js/trademark-similarity/run-search.js
@@ -0,0 +1,27 @@
+import algoliasearch from 'algoliasearch';
+import { calculateSimilarityScore } from './scorer.js';
+import { isValidBasedOnDate, hasOverlappingNiceClasses } from './filters.js';
+
+const client = algoliasearch('YourAppId', 'YourSearchApiKey');
+const index = client.initIndex('trademark_bulletin_records_live');
+
+export async function runTrademarkSearch(monitoredMark, selectedBulletinNo) {
+  const { markName, applicationDate, niceClasses } = monitoredMark;
+
+  const { hits } = await index.search(markName, {
+    filters: `bulletinNo:${selectedBulletinNo}`,
+    getRankingInfo: true,
+    hitsPerPage: 1000
+  });
+
+  const enriched = hits
+    .filter(hit => isValidBasedOnDate(hit.applicationDate, applicationDate))
+    .map(hit => {
+      const similarityScore = calculateSimilarityScore(hit, markName);
+      const sameClass = hasOverlappingNiceClasses(hit.niceClasses || [], niceClasses);
+      return { ...hit, similarityScore, sameClass };
+    })
+    .sort((a, b) => b.similarityScore - a.similarityScore);
+
+  return enriched;
+}
