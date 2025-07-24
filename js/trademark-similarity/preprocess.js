08eda8de687d6900f678f1418695f0987fb9674d
kubilayguzel <mkubilayguzel@gmail.com>
2025-07-24 13:46:18 +0300

    Update preprocess.js

--- js/trademark-similarity/preprocess.js
+++ js/trademark-similarity/preprocess.js
@@ -1,7 +1,11 @@
 export const GENERIC_WORDS = [
   "turizm", "tekstil", "gıda", "inşaat", "danışmanlık", "hizmet",
   "bilişim", "teknoloji", "sanayi", "ticaret", "sigorta",
-  "yayıncılık", "grup", "şirketi", "ltd", "aş", "anonim", "mobilya", "otomotiv"
+  "yayıncılık", "grup", "şirketi", "ltd", "aş", "anonim", "mobilya", "otomotiv", 
+  "kalite", "ürün", "ürünler", "pazarlama", "dekorasyon", "inovasyon",
+  "endüstri", "enerji", "kimya", "makine", "makina", "giyim", "elektrik", "yapı",
+  "proje", "dizayn", "tasarım", "lojistik", "ulaşım", "kargo", "taşımacılık",
+  "yazılım", "market", "mağaza",  "mağazacılık", "otomotiv", "tarım", "sağlık"
 ];
 
 export function cleanMarkName(name) {
